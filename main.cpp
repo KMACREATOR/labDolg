eaa20f7e08edb13387f736964b21e667034d7884
KMACREATOR <agredi2567@gmail.com>
2024-06-20 02:25:24 +0300

    added main

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+setlocale(LC_ALL, "RUSSIAN");
+int n;
+cout << "Введите n:\t";
+cin >> n;
+for (int i = 0; i < n; ++i){
+cout << "I will never loose deadline!\n";
+return 0;
+}
+}
